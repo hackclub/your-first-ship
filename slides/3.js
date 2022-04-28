76115ae5fa64294e918f0e758a2d01215f8a906b
Belle <bellesee1212@gmail.com>
2022-04-28 20:42:17 +0800

    switch from clippy to toriel

--- slides/3.js
+++ slides/3.js
@@ -17,13 +17,16 @@ export default function SlideThree() {
       <Box>
         <Box
           sx={{
+            bg: 'white',
+            p: 3,
             mb: 4,
+            borderRadius: 6,
             width: 'fit-content',
             mx: 'auto'
           }}
         >
           <img
-            src="https://i.gifer.com/origin/c6/c6afab251a20e6d0eb80b983450bc66e_w200.gif"
+            src="/toriel.png"
             height="300"
             width="300"
           />
@@ -33,10 +36,10 @@ export default function SlideThree() {
           color="white"
           sx={{ fontSize: [6, 7], fontWeight: 900 }}
         >
-          2: Meet Clippy
+          2: Meet Toriel
         </Heading>
         <Heading sx={{ fontWeight: 400 }}>
-          Clippy will show you around the Slack and all of it's channels!
+          Toriel will show you around the Slack and all of it's channels!
         </Heading>
       </Box>
     </Box>
